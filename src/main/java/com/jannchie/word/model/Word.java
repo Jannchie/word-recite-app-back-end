e0af8617e29096df5ea108e3e96e7d4f284bf095
jannchie <jannchie@gmail.com>
2020-01-21 01:37:08 +0900

    ADD: word model

--- src/main/java/com/jannchie/word/model/Word.java
+++ src/main/java/com/jannchie/word/model/Word.java
@@ -1,5 +1,36 @@
 package com.jannchie.word.model;
 
-public class Word {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Document
+public class Word {
+    private String word;
+    private String definition;
+    private String translation;
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+
+    public String getTranslation() {
+        return translation;
+    }
+
+    public void setTranslation(String translation) {
+        this.translation = translation;
+    }
 }
