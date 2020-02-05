e88d762f4690431aba3b36449613d57291f82ea6
jannchie <jannchie@gmail.com>
2020-02-05 21:38:10 +0900

    UPDATE: Result model

--- src/main/java/com/jannchie/word/model/Result.java
+++ src/main/java/com/jannchie/word/model/Result.java
@@ -1,18 +1,26 @@
 package com.jannchie.word.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.jannchie.word.constant.ResultEnum;
+import org.springframework.http.ResponseEntity;
 
 /**
  * @author Jannchie
  */
-public class Result {
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Result<T>{
     private String msg;
     public Result(String msg) {
         this.msg = msg;
     }
 
-    public Result(ResultEnum loginFaild) {
-        this.msg = loginFaild.getMsg();
+    public Result(ResultEnum resultEnum) {
+        this.msg = resultEnum.getMsg();
+    }
+    private T data;
+    public Result(ResultEnum resultEnum, T data) {
+        this.msg = resultEnum.getMsg();
+        this.data = data;
     }
 
     public String getMsg() {
@@ -21,5 +29,13 @@ public class Result {
 
     public void setMsg(String msg) {
         this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
     }
 }
